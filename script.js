17ca0286f53f10854d8edd3bd0ea791de0a10744
ZubairARooghwall <99389773+ZubairARooghwall@users.noreply.github.com>
2023-02-20 17:40:50 +0430

    Add the ability to resize

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let divContainer = document.getElementById("container");
-
+let prevSize;
 
 //append the divs to the container as children
 function createDivs(rows){
     for(let a = 0; a < rows * rows; a++){
-            let createDiv = document.createElement("div");
-            divContainer.appendChild(createDiv).className = "children";
+        let createDiv = document.createElement("div");
+        divContainer.appendChild(createDiv).className = "children";
  }
+    prevSize = rows;
 }
 
 //call the createChildren function
-createDivs(16)
+createDivs(16);
 
 //add eventlistener to ever div in the container
 let children = document.querySelectorAll(".children");
@@ -26,5 +27,21 @@ children.forEach((item) => {
 
 
 
+function resize(){
+    let size = prompt("Enter the size: ");
+
+    let childContainer = divContainer.lastChild;
+
+    while(childContainer){
+        divContainer.removeChild(childContainer);
+        childContainer = divContainer.lastChild;
+    }
+
+    createDivs(size);
+
+
+
+
+}
 
 
