bd35b4dc433c9aee78a90c2389c6167791ebd490
ZubairARooghwall <99389773+ZubairARooghwall@users.noreply.github.com>
2023-02-21 09:40:47 +0430

    Add the ability to generate random number

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 let divContainer = document.getElementById("container");
-let prevSize;
+let elementSize;
+
 
 //append the divs to the container as children
 function createDivs(rows){
-    for(let a = 0; a < rows * rows; a++){
-        let createDiv = document.createElement("div");
-        divContainer.appendChild(createDiv).className = "children";
+    for(let a = 0; a < rows; a++){
+        for(let b = 0; b < rows; b++){
+            let createDiv = document.createElement("div");
+            divContainer.appendChild(createDiv).className = "children";  
+
  }
-    prevSize = rows;
+
+} 
+
+    divContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
+    divContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+
+
+
+let children = document.querySelectorAll(".children");
+children.forEach((item) => {
+    let checkBoxRandom = document.getElementById("Random");
+    let checkBoxGrey = document.getElementById("gray");
+    item.addEventListener('mouseover', (e) =>{
+        console.log("hovered")
+        console.log(checkBoxGrey)
+        console.log(checkBoxRandom)
+        if(checkBoxGrey.checked == false 
+            & checkBoxRandom.checked == false 
+            || (checkBoxGrey.checked == false && checkBoxRandom.checked == false)){
+        item.classList.add("hovered");
+        }
+        else if(checkBoxRandom.checked == true){
+            checkBoxGrey.checked = false;
+            item.setAttribute("style", `background-color: rgb(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`);
+            console.log(item.getAttribute("style"))
+        }
+        else if(checkBoxGrey.checked == true){
+            checkBoxRandom.checked = false;
+            
+
+        }
+
+    })
+
+});
+
+    
+
 }
 
 //call the createChildren function
 createDivs(16);
 
 //add eventlistener to ever div in the container
-let children = document.querySelectorAll(".children");
-children.forEach((item) => {
-    item.addEventListener('mouseover', (e) =>{
-        console.log("hovered")
-        item.classList.add("hovered");
-        
-
-    })
-
-});
-
 
 
 function resize(){
-    let size = prompt("Enter the size: ");
+    let size = prompt("Enter the size: (1 <= size <= 100");
 
+    
+    elementSize = size;
     let childContainer = divContainer.lastChild;
 
     while(childContainer){
@@ -45,3 +76,8 @@ function resize(){
 }
 
 
+function generateRandomNumber(){
+    let number = Math.floor(Math.random() * 255) + 1;
+    console.log(`Random: ${number}`);
+    return number;
+}
